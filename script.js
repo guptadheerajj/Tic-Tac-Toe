faed08f89c74c91a07f21782fadc2af2333adde2
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-03-19 18:44:37 +0530

    make playRound logic of gameController module

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*
- ** represents the state of the board, like
+ ** gameBoard module represents the state of the board, like
  ** which cell has which player's Mark
  ** which is cell is empty, available to use
  */
@@ -10,7 +10,7 @@ const gameBoard = (() => {
 	// represents each cell in the game board
 	function cell() {
 		let value = 0;
-		addMark = (player) => (value = player.mark);
+		addMark = (playerMark) => (value = playerMark);
 		getValue = () => value;
 
 		return {
@@ -18,20 +18,26 @@ const gameBoard = (() => {
 			getValue,
 		};
 	}
+
 	for (let row = 0; row < rows; row++) {
 		board[row] = [];
 		for (let column = 0; column < columns; column++) {
 			board[row].push(cell());
 		}
 	}
+
 	const getBoard = () => board;
-	const markCell = (row, column, player) => {
+
+	const markCell = (row, column, playerMark) => {
 		const targetCell = board[row - 1][column - 1];
 		const isCellAvailable = targetCell.getValue() === 0 ? true : false;
-		if (!isCellAvailable) return;
-
-		targetCell.addMark(player);
+		if (isCellAvailable) {
+			targetCell.addMark(playerMark);
+			return isCellAvailable;
+		}
+		return isCellAvailable;
 	};
+
 	const printBoard = () => {
 		for (let row = 0; row < rows; row++) {
 			let rowValues = board[row].map((rowCell) => rowCell.getValue());
@@ -46,8 +52,13 @@ const gameBoard = (() => {
 	};
 })();
 
-// control the flow of the game, manage the turns of players and check for wins
+/*
+ ** gameController module represents the flow of the game,
+ ** manage the turns of players
+ ** check for win condition
+ */
 const gameController = (function () {
+	// player factory function with score and turn as private variables
 	function player(name, mark, turn) {
 		let score = 0;
 		const getScore = () => score;
@@ -60,28 +71,50 @@ const gameController = (function () {
 		const getTurn = () => turn;
 		return { name, mark, getScore, incrementScore, toggleTurn, getTurn };
 	}
-	return {player}
+
+	function playRound(row, column) {
+		const playerTurn = players[0].getTurn() ? players[0] : players[1];
+		const markStatus = gameBoard.markCell(row, column, playerTurn.mark);
+
+		if (markStatus) {
+			players.map((eachPlayer) => {
+				eachPlayer.toggleTurn();
+			});
+		}
+	}
+
+	return { player, playRound };
 })();
 
 const players = [
-	gameController.player("Dheeraj", "X", true),
-	gameController.player("Bot", "O", false),
+	gameController.player("Dheeraj", "X", false),
+	gameController.player("Bot", "O", true),
 ];
+
 gameBoard.printBoard();
-gameBoard.markCell(1, 1, { mark: "X" });
+gameController.playRound(1, 1);
 gameBoard.printBoard();
-gameBoard.markCell(3, 3, { mark: "O" });
+gameController.playRound(1, 2);
 gameBoard.printBoard();
-gameBoard.markCell(1, 3, { mark: "X" });
+gameController.playRound(3, 1);
 gameBoard.printBoard();
-gameBoard.markCell(1, 2, { mark: "O" });
+gameController.playRound(2, 2);
 gameBoard.printBoard();
-gameBoard.markCell(3, 1, { mark: "X" });
+gameController.playRound(2, 1);
 gameBoard.printBoard();
-gameBoard.markCell(2, 2, { mark: "O" });
-gameBoard.printBoard();
-gameBoard.markCell(2, 1, { mark: "X" });
+gameController.playRound(3, 2);
 gameBoard.printBoard();
 
-// controls the display of board in user interface
-(function screenController() {})();
+// gameBoard.markCell(1, 3, { mark: "X" });
+// gameBoard.printBoard();
+// gameBoard.markCell(1, 2, { mark: "O" });
+// gameBoard.printBoard();
+// gameBoard.markCell(3, 1, { mark: "X" });
+// gameBoard.printBoard();
+// gameBoard.markCell(2, 2, { mark: "O" });
+// gameBoard.printBoard();
+// gameBoard.markCell(2, 1, { mark: "X" });
+// gameBoard.printBoard();
+
+// // controls the display of board in user interface
+// (function screenController() {})();
